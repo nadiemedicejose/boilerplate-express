5ff72ba558adec30d7332ecfacb92e70d8bd375a
Jose Estrada <jose@nadiemedicejose.com>
2021-06-22 23:25:04 -0700

    Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 
 // TODO: Log "Hello World" to the console.
-console.log("Hello World");
+// console.log("Hello World");
+
+// TODO: Use the app.get() method to serve the string "Hello Express" to GET requests matching the / (root) path. Be sure that your code works by looking at the logs.
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
 module.exports = app;
