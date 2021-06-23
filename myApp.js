8ea8d61f0f38f9ff0ae47e358679fcb85ef7d4c9
Jose Estrada <jose@nadiemedicejose.com>
2021-06-22 23:22:21 -0700

    Log "Hello World" to the console

--- myApp.js
+++ myApp.js
@@ -1,39 +1,7 @@
 var express = require('express');
 var app = express();
 
+// TODO: Log "Hello World" to the console.
+console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
